5c0d136aaec4878e1bb95b72f0316a59c94bbd8e
Marmiz <claudio.restifo@gmail.com>
2016-10-10 18:09:31 +0700

    heroku

--- server.js
+++ server.js
@@ -21,6 +21,6 @@ MongoClient.connect(url, function(err, db){
 
 
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(process.env.PORT || 3000, function () {
+  console.log('ready to serve!');
 });
